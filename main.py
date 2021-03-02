aaab8caa5475306ae3ae32b6a98cb466b23a32ba
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-03-02 12:11:55 +0100

    SMTP set up

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import smtplib
 
 my_email = "bendzandu117@gmail.com"
+password = "ThisIsATest"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="bendzandu117@yahoo.com",
+                        msg="Subject:The Second\n\nThis is the body of the email")
