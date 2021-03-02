5977d738d444bd2232c33a67f570bff77cf8a771
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-03-02 12:47:44 +0100

    Importing text file

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import random
 import smtplib
+import datetime as dt
 
 my_email = "bendzandu117@gmail.com"
 password = "ThisIsATest"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="bendzandu117@yahoo.com",
-                        msg="Subject:The Second\n\nThis is the body of the email")
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quote = quote_file.readlines()
+        quote = random.choice(all_quote)
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="bendzandu117@yahoo.com",
+                            msg=f"Subject:Monday Words of Wisdom\n\n{quote}")
+
